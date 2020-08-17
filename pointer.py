769c4aff3cd02832a80e480a04d341d368418ec3
Edotnd <edotnd@gmail.com>
2020-08-18 00:20:38 +0900

    version -1.2.1-p

--- pointer.py
+++ pointer.py
@@ -3,12 +3,13 @@ listx = [11, 23, 42, 423, 5, 75]
 class P:
     def __init__(self, mem_p, num=0):
         self.mem_p = mem_p
+        self.origin_file = mem_p
         self.Next_P = {num : self.mem_p[num]}
         self.num = num
         self.mem_init_n = 0
     def __add__(self, slice_number=0):
         i = int(self.num + slice_number)
-        j =  i-(len(listx)-1) if len(listx)-1 < i else 0
+        j =  i-(len(self.origin_file)-1) if len(self.origin_file)-1 < i else 0
         self.mem_init_n += j - 1
         self.mem_p += ['0x'+str(z) for z in range(self.mem_init_n, j+self.mem_init_n)]
         self.Next_P = {i : self.mem_p[i]}
@@ -16,5 +17,3 @@ class P:
 
 p1 = P(listx)
 print(p1.Next_P)
-
-
