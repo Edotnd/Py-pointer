1166b892578174d7b77165e527c2e39b41aab4bb
Edotnd <edotnd@gmail.com>
2020-08-18 18:37:05 +0900

    version -2.0.1-p

--- pointer.py
+++ pointer.py
@@ -7,12 +7,13 @@ class P:
         self.num = num
         self.Next_P = {num : self.mem_p[num]}
         self.origin_len = len(mem_p)
+        self.sub_len = 0
         
 
     def __add__(self, slice_number=0):
         i = int(self.num + slice_number)
         if i > len(self.mem_p)-1:
-            self.mem_p += ['0x'+str(z) for z in range(len(self.mem_p)-self.origin_len, i-self.origin_len+1)]
+            self.mem_p += ['0x'+str(z+self.sub_len) for z in range(len(self.mem_p)-self.origin_len, i-self.origin_len+1)]
             self.Next_P = {len(self.mem_p)-1 : self.mem_p[-1]}
             self.num = len(self.mem_p)-1
         else:
@@ -24,12 +25,12 @@ class P:
         self.Next_P = {self.num : self.mem_p[self.num]}
 
     def __sub__(self, slice_number=0):
-        i = int(self.num - slice_number)+1
+        i = int(self.num-slice_number)
         if i < 0:
-            self.mem_p = ['-0x'+str(-z) for z in range(i, 1)]+self.mem_p
+            self.mem_p = ['-0x'+str(-z) for z in range(i+1, self.sub_len+1)] + self.mem_p
+            self.sub_len = i
             self.Next_P = {0 : self.mem_p[0]}
-            self.num = 0
         else:
-            self.num -= slice_number
-            self.Next_P = {self.num : self.mem_p[self.num]}
+            pass
+            
 
